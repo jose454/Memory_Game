994521c999251160b475fa3de60c43b548881a3e
Jose Ricardo <cadinhosjp@gmail.com>
2020-01-06 19:49:36 -0300

    Randomização das cartas

---
+++ script.js
@@ -0,0 +1,71 @@
+var cards = new Array(16);
+var k;
+
+function create_cards (){
+    for (k = 0 ; k < 16 ; k++){
+        cards[k] = {
+            div: document.createElement('div'),
+            background: 'white',
+            value: 0};//value 0, a carta está virada, value 1, a carta está desvirada
+        cards[k].div.setAttribute('class', 'cards');
+        document.getElementById('game-field').appendChild(cards[k].div);
+    }
+    define_bakground_cards();
+}
+
+var par = 0;
+var cont = 0;
+var card;
+
+function define_bakground_cards (){
+    for (k = 0 ; k < 16 ; k++){
+        cont++;
+        //Math.floor(Math.random() * (max - min + 1) + min)
+        card = Math.floor(Math.random() * (15 - 0 + 1) + 0);
+        if (cards[card].background === 'white'){
+            switch (par){
+                case 0:
+                    cards[card].background = 'url(./img/angular.png)';
+                    break;
+                case 1:
+                    cards[card].background = 'url(./img/java.png)';
+                    break;
+                case 2:
+                    cards[card].background = 'url(./img/javascript.png)';
+                    break;
+                case 3:
+                    cards[card].background = 'url(./img/c.png)';
+                    break;
+                case 4:
+                    cards[card].background = 'url(./img/python.png)';
+                    break;
+                case 5:
+                    cards[card].background = 'url(./img/node.png)';
+                    break;
+                case 6:
+                    cards[card].background = 'url(./img/vue.png)';
+                    break;
+                case 7:
+                    cards[card].background = 'url(./img/react.png)';
+                    break;
+            }
+            if (cont == 2){
+                par++;
+                cont = 0;
+            }
+        }
+        else {
+            k--;
+            cont--;
+        }
+    }
+    set_colors();
+}
+
+function set_colors (){
+    for (k = 0 ; k < 16 ; k++){
+        cards[k].div.style.background = cards[k].background;
+        cards[k].div.style.backgroundSize = 'cover';
+        cards[k].div.style.backgroundPosition = 'center';
+    }
+}
