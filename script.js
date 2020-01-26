63cafbdd7df6bb0ef0228d2211a48dc9be2ca306
Jose Ricardo <cadinhosjp@gmail.com>
2020-01-26 16:17:44 -0300

    Imagem do fundo da carta e novas funções como, a função de finalizar o jogo foram adiconadas

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var cards = [];
 
+var numero_de_jogadas = 16;
+
+var pares_nao_encontrados = 8;
+
+var info_jogadas_restantes = document.getElementById('jogadas-restantes');
+info_jogadas_restantes.innerText = numero_de_jogadas;
+
 var gameField = document.getElementById('game-field');
 gameField.onload = create_cards();
 
@@ -81,6 +88,11 @@ function ativaClasse(i) {
     cards[i].div.classList.toggle('active');
 }
 
+function desativaClasse(i) {
+    cards[i].div.style.background = 'url(./img/back.png)';
+    cards[i].div.style.backgroundSize = 'cover';
+}
+
 function showAllCards() {
     for (var k=0;k<16;k++) {
         ativaClasse(k);
@@ -90,6 +102,7 @@ function showAllCards() {
 function hideCards() {
     for (var k=0;k<16;k++) {
         cards[k].div.classList.remove('active');
+        desativaClasse(k);
     }
 }
 
@@ -148,12 +161,49 @@ function verificarIgualdade(arr) {
     if(arr[0].id === arr[1].id) {
         console.log('acertou');
         arr[0].div.removeAttribute('onclick');
+        arr[0].value = 1;
         arr[1].div.removeAttribute('onclick');
+        arr[1].value = 1;
+        pares_nao_encontrados--;
     }
     else {
         setTimeout(function(){
             arr[0].div.classList.remove('active');
+            arr[0].div.style.background = 'url(./img/back.png)';
+            arr[0].div.style.backgroundSize = 'cover';
             arr[1].div.classList.remove('active');
+            arr[1].div.style.background = 'url(./img/back.png)';
+            arr[1].div.style.backgroundSize = 'cover';
         },500);
     }
+    if (numero_de_jogadas > 0){
+        numero_de_jogadas--;
+    }
+    if (numero_de_jogadas == 0) {
+        end_game();
+    }
+    if (pares_nao_encontrados == 0) {
+        end_game();
+    }
+    info_jogadas_restantes.innerText = numero_de_jogadas;
+}
+
+function end_game (){
+    let cartas_nao_descobertas = 0;
+    for (k = 0 ; k < 16 ; k++){
+        if (cards[k].value == 0){
+            cards[k].div.removeAttribute('onclick');
+            cartas_nao_descobertas++;
+        }
+    }
+    if (cartas_nao_descobertas == 0){
+        alert('Você ganhou!, conseguiu encontrar todos os pares');
+    }
+    else {
+        alert('Você perdeu!, não conseguiu encontrar todos os pares');
+    }
+}
+
+function reload (){
+    window.location.reload();
 }
